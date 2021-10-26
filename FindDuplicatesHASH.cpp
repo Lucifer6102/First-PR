b3afc455f2c50304277a93c58120f7468f2e9d7d
avichalsikka <73197328+avichalsikka@users.noreply.github.com>
2021-10-26 22:05:53 +0530

    Update FindDuplicatesHASH.cpp

--- FindDuplicatesHASH.cpp
+++ FindDuplicatesHASH.cpp
@@ -5,17 +5,13 @@ int main(){
 
     char input[100];
     int hash[26];
-
     cout<<"Enter String ";
     gets(input);
     puts(input);
-
     int i=0,j=0;
-
     for(int j=0;j<=25;j++){
         hash[j]=0;
     }
-
     while(input[i]!='\0'){
         hash[input[i]-97]+=1;
         if(hash[input[i]-97]==2){
